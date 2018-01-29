2ea70642b0b7342747a05319ef1778d97be07c60
AlirieGray <caitlin.estrellita@gmail.com>
2018-01-29 14:31:21 -0800

    added components to main app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Menu from './components/Menu'
+import Receipt from './components/Receipt'
 
 class App extends Component {
   render() {
     return (
       <div>
-        
+        <Menu />
+        <Receipt />
       </div>
     );
   }
--- src/reducers/burger.js
+++ src/reducers/burger.js
@@ -1,49 +1,8 @@
-const burgerDefault = {
-  protein: 'Beef',
-  weight: '1/3 lb',
-  bun: 'Brioche Style Bun',
-  cheese: [],
-  toppings: [],
-  premium: [],
-  sauces: []
-}
+const burgerDefault = [];
 
 export default (state = burgerDefault, action) => {
   switch (action.type) {
-    case PICK_PROTEIN:
-      return {
-        ...state,
-        protein: action.protein
-      };
-    case PICK_WEIGHT:
-      return {
-        ...state,
-        weight: action.weight
-      };
-    case PICK_BUN:
-      return {
-        ...state,
-        bun: action.bun
-      };
-    case ADD_CHEESE:
-      return {
-        ...state,
-        cheese: [...state.cheese, action.cheese]
-      };
-    case ADD_TOPPING:
-      return {
-        ...state,
-        toppings: [...state.toppings, action.topping]
-      };
-    case ADD_PREMIUM:
-      return {
-        ...state,
-        premium: [...state.premium, action.premium]
-      };
-    case ADD_SAUCE:
-      return {
-        ...state,
-        sauces: [...state.sauces, action.sauces]
-      }
+    case 'ADD_ITEM':
+      return [...state, action.item]
   }
 }
